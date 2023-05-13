cc0101b8df1c8a3631827a5a959e81215169fc9c
junkyo <124774799+junkyo974@users.noreply.github.com>
2023-05-13 19:34:02 +0900

    feat: 토큰만료 시 재발급 수정, 시크릿키 환경변수 참조하도록 수정 : 이준교

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken")
 const {Users} = require("../models");
+const redisClient = require("../utils/redis");
 
 module.exports = async (req, res, next) => {
     
@@ -24,35 +25,43 @@ module.exports = async (req, res, next) => {
     } catch (error){
         if (error.name === "TokenExpiredError") {
             const refreshToken = req.cookies.refreshToken;
+            const redisRefreshToken = await redisClient.get(user_id);
+      
+            if (refreshToken !== redisRefreshToken) {
+              return res.status(401).json({
+                errormessage: "토큰이 일치하지 않습니다. 다시 로그인해 주세요.",
+              });
+            }
+      
             const decodedRefreshToken = jwt.verify(
-                refreshToken,
-                process.env.REFRESH_SECRET_KEY
+              refreshToken,
+              process.env.REFRESH_SECRET_KEY
             );
             const userId = decodedRefreshToken.userId;
-
-            const user = await Users.findOne({ where: { userId } });
+      
+            const user = await Users.findOne({ where: { user_id } });
             if (!user) {
-                return res.status(401).json({
-                    errormessage: "리프레시 토큰에 해당하는 사용자가 존재하지 않습니다.",
-                });
+              return res.status(401).json({
+                errormessage: "리프레시 토큰에 해당하는 사용자가 존재하지 않습니다.",
+              });
             }
-
+      
             const newAccessToken = jwt.sign(
-                { userId: user.userId },
-                process.env.SECRET_KEY,
-                { expiresIn: "2h" }
+              { userId: user.userId },
+              process.env.SECRET_KEY,
+              { expiresIn: "2h" }
             );
-
+      
             res.cookie("authorization", `Bearer ${newAccessToken}`, {
-                httpOnly: true,
-                secure: true,
+              httpOnly: true,
+              secure: false,
             });
             res.locals.user = user;
             return next();
-        } else {
+          } else {
             return res.status(403).json({
-                errormessage: "전달된 쿠키에서 오류가 발생하였습니다.",
+              errormessage: "전달된 쿠키에서 오류가 발생하였습니다.",
             });
+          }
         }
-    }
-};
+      };
