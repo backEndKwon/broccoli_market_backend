cbd037ba7ff8993bb72f08e48a2aa98550d40526
backEndKwon <rhaehfdl0433@gmail.com>
2023-05-17 09:32:39 +0900

    fix: mypage URL수정, testCode 추가

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,6 +4,7 @@ const path = require('path');
 const ejs = require('ejs');
 const appDir = path.dirname(require.main.filename);
 const redisClient = require('../utils/redis.js')
+require("dotenv").config();
 
 class AuthController {
     authService = new AuthService();
